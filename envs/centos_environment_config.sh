f347df054bc4b5d9a89fb70f2c7b6214518280b0
Cyril.J <juc_golden@outlook.com>
2020-06-08 00:42:54 +0800

    Update centos_environment_config.sh

--- envs/centos_environment_config.sh
+++ envs/centos_environment_config.sh
@@ -12,7 +12,6 @@ ID=$(cat /etc/os-release | grep "^ID=" | cut -d\" -f2)  ## centos
 VERSION_ID=$(cat /etc/os-release | grep "^VERSION_ID=" | cut -d\" -f2) ## 7/8
 
 
-
 ###########################################
 ###@repo源 更换为 Ali源
 # 1. centos7
@@ -22,53 +21,28 @@ case "${ID}${VERSION_ID}" in
 "centos7")
     if [ ! -f "/etc/yum.repos.d/CentOS-Base.repo.bak" ];then
         mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
-    curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
     fi
-
+    curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-8.repo
+    yum clean all
+    yum makecache
 ;;
 "centos8")
-    cat <<EOF > /etc/yum.repos.d/CentOS-AppStream.repo
-# file: /etc/yum.repos.d/CentOS-AppStream.repo
-[AppStream]
-name=CentOS-$releasever - AppStream
-baseurl=http://mirrors.aliyun.com/centos/$releasever/AppStream/$basearch/os/
-gpgcheck=1
-enabled=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
-EOF
-    cat <<EOF > /etc/yum.repos.d/CentOS-Base.repo
-# file: /etc/yum.repos.d/CentOS-Base.repo
-[BaseOS]
-name=CentOS-$releasever - Base
-baseurl=http://mirrors.aliyun.com/centos/$releasever/BaseOS/$basearch/os/
-gpgcheck=1
-enabled=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
-EOF
-    cat <<EOF > /etc/yum.repos.d/CentOS-Epel.repo
-# file: /etc/yum.repos.d/CentOS-Epel.repo
-[epel]
-name=CentOS-$releasever - Epel
-baseurl=http://mirrors.aliyun.com/epel/8/Everything/$basearch
-enabled=1
-gpgcheck=0
-EOF
-    cat <<EOF > /etc/yum.repos.d/CentOS-Media.repo
-# file: /etc/yum.repos.d/CentOS-Media.repo
-# 注：CentOS-Media 配置需要将光盘挂载至/media/CentOS路径，否则在使用时会报错。
-[c8-media-BaseOS]
-name=CentOS-BaseOS-$releasever - Media
-baseurl=file:///media/CentOS/BaseOS/
-gpgcheck=1
-enabled=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
-[c8-media-AppStream]
-name=CentOS-AppStream-$releasever - Media
-baseurl=file:///media/CentOS/AppStream/
-gpgcheck=1
-enabled=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
-EOF
+    if [ ! -f "/etc/yum.repos.d/CentOS-AppStream.repo.bak" ];then
+        mv /etc/yum.repos.d/CentOS-AppStream.repo /etc/yum.repos.d/CentOS-AppStream.repo.bak
+    fi
+    if [ ! -f "/etc/yum.repos.d/CentOS-Base.repo.bak" ];then
+        mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+    fi
+    if [ ! -f "/etc/yum.repos.d/CentOS-Epel.repo.bak" ];then
+        mv /etc/yum.repos.d/CentOS-Epel.repo /etc/yum.repos.d/CentOS-Epel.repo.bak
+    fi
+    if [ ! -f "/etc/yum.repos.d/CentOS-Media.repo.bak" ];then
+        mv /etc/yum.repos.d/CentOS-Media.repo /etc/yum.repos.d/CentOS-Media.repo.bak
+    fi
+    
+    curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-8.repo
+    yum clean all
+    yum makecache
 ;;
 *)
     echo "not support system version [${ID}${VERSION_ID}], repoes not be updated."
